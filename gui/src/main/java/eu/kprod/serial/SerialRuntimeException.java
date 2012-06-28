025c32264a75ca37300578d3589fcdff1db9be9c
treym <treym@localhost>
2012-06-28 23:32:45 +0200

    new file

---
+++ gui/src/main/java/eu/kprod/serial/SerialRuntimeException.java
@@ -0,0 +1,17 @@
+package eu.kprod.serial;
+
+import java.io.UnsupportedEncodingException;
+
+public class SerialRuntimeException extends RuntimeException {
+
+    public SerialRuntimeException(String string, UnsupportedEncodingException a1) {
+        // TODO Auto-generated constructor stub
+        super(string, a1);
+    }
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+}
